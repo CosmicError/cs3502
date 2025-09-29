9d76d6effb345bdb449537e42e00e6ed098bb750
CosmicError <47365403+CosmicError@users.noreply.github.com>
2025-09-29 14:23:06 -0400

    Finished phase 1 and started phase 2

--- project1/phase1.c
+++ project1/phase1.c
@@ -5,10 +5,13 @@
 #include <time.h>
 #include <string.h>
 #include <errno.h>
+#include <math.h>
 
 #define NUM_ACCOUNTS 1
-const int NUM_THREADS = 10;
-const int TRANSACTIONS_PER_TELLER = 10;
+#define NUM_THREADS 10
+#define TRANSACTIONS_PER_TELLER 10
+
+const int INITIAL_BALANCE = 1000;
 
 // Shared data structure
 typedef struct {
@@ -18,38 +21,27 @@ typedef struct {
 } Account;
 
 Account accounts[NUM_ACCOUNTS];
+double logs[NUM_THREADS][TRANSACTIONS_PER_TELLER];
 
 // Thread function
-void* teller_thread (void * arg) {
+void* teller_thread(void * arg) {
     int teller_id = *(int*) arg; // Cast void * to int * and dereference
+    unsigned int seed = time(NULL) + pthread_self() ;
 
     // Perform multiple transactions
     for (int i = 0; i < TRANSACTIONS_PER_TELLER; i++) {
 
         // Select random account
-        // int random_account;
-        // do {
-        unsigned int seed = time(NULL) + pthread_self() ;
-        int random_account = rand_r(&seed) % NUM_ACCOUNTS;
-        // } while (accounts[random_account] == NULL);
+        int random_account = rand_r(&seed) % NUM_ACCOUNTS; 
 
+        // only 1 account so not much to do with diff
+        double diff = 100;
 
-        // Perform deposit or withdrawal
-        switch (random_account % 4)
-        {
-        case 0:
-            accounts[random_account].balance -= 100;
-            break;
-        case 1:
-            accounts[random_account].balance += 100 ;
-            break;
-        case 2:
-            accounts[random_account].balance -= 50;
-            break;
-        case 3:
-            accounts[random_account].balance += 50;
-            break;
-        }
+        accounts[random_account].balance += diff;
+        accounts[random_account].transaction_count++;
+
+        logs[teller_id][i] = diff;
+        printf("Thread %d: %s %f\n", teller_id, (diff < 0)? "Withdrawing" : "Depositing", fabs(diff));
 
         // THIS WILL HAVE RACE CONDITIONS !
 
@@ -64,10 +56,12 @@ int main() {
     for (int i = 0; i < NUM_ACCOUNTS; i++) {
         accounts[i] = (Account){
             .account_id = i, 
-            .balance = 1000, 
+            .balance = INITIAL_BALANCE, 
             .transaction_count = 0
         };
     }
+
+    printf("Initial Balance: %f\n", accounts[0].balance);
     
     // Creating threads ( see Appendix \ ref { sec : voidpointer } for void * explanation )
     pthread_t threads [NUM_THREADS];
@@ -75,13 +69,24 @@ int main() {
 
     for (int i = 0; i < NUM_THREADS; i++) {
         thread_ids[i] = i;
-        pthread_create (&threads[i], NULL, teller_thread, &thread_ids[i]);
+        pthread_create(&threads[i], NULL, teller_thread, &thread_ids[i]);
     }
 
     // Wait for all threads to complete
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
+
+    printf("Final Account Balances: %f\n", accounts[0].balance);
+
+    double sum = INITIAL_BALANCE; // starting account balance
+    for (int i = 0; i < NUM_THREADS; i++) {
+        for (int j = 0; j < TRANSACTIONS_PER_TELLER; j++) {
+            sum += logs[i][j];
+        }
+    }
+
+    printf("Correct Account Balance: %f\n\n", sum);
 
     return 0;
 }
---
+++ project1/phase2.c
@@ -0,0 +1,122 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
+#include <math.h>
+
+#define NUM_ACCOUNTS 1
+#define NUM_THREADS 10
+#define TRANSACTIONS_PER_TELLER 10
+
+const double INITIAL_BALANCE = 1000;
+
+// Shared data structure
+typedef struct {
+    int account_id ;
+    double balance ;
+    int transaction_count;
+    pthread_mutex_t lock;
+} Account;
+
+Account accounts[NUM_ACCOUNTS];
+double logs[NUM_THREADS][TRANSACTIONS_PER_TELLER];
+
+int deposit(int account_id, double amount) {
+    pthread_mutex_lock(&accounts[account_id].lock);
+
+    if (pthread_mutex_lock(&accounts[account_id].lock) != 0) {
+        perror("Failed to acquire lock");
+        return 0;
+    }
+
+    accounts[account_id].balance += amount;
+    accounts[account_id].transaction_count++;
+
+    pthread_mutex_unlock(&accounts[account_id].lock);
+
+    return 1;
+}
+
+// Thread function
+void* teller_thread(void * arg) {
+    int teller_id = *(int*) arg; // Cast void * to int * and dereference
+
+    int result = 1;
+    unsigned int seed;
+    int random_account;
+
+    // Perform multiple transactions
+    for (int i = 0; i < TRANSACTIONS_PER_TELLER; i++) {
+
+        // Select random account
+        if (result) {
+            seed = time(NULL) + pthread_self();
+            random_account = rand_r(&seed) % NUM_ACCOUNTS; 
+        }  
+
+        // only 1 account so not much to do with diff
+        double amount = 100;
+
+        result = deposit(random_account, amount);
+
+        if (!result) {
+            i--;
+            continue;
+        }
+
+        logs[teller_id][i] = amount;
+
+        printf("Thread %d: %s %f\n", teller_id, (amount < 0)? "Withdrawing" : "Depositing", fabs(amount));
+
+        // THIS WILL HAVE RACE CONDITIONS !
+
+        printf ("Teller %d: Transaction %d\n", teller_id, i) ;
+
+    }
+
+    return NULL ;
+}
+
+int main() {
+    for (int i = 0; i < NUM_ACCOUNTS; i++) {
+        accounts[i] = (Account){
+            .account_id = i, 
+            .balance = INITIAL_BALANCE, 
+            .transaction_count = 0
+        };
+
+        pthread_mutex_init(&accounts[i].lock, NULL);
+    }
+
+    printf("Initial Balance: %f\n", accounts[0].balance);
+    
+    // Creating threads ( see Appendix \ ref { sec : voidpointer } for void * explanation )
+    pthread_t threads [NUM_THREADS];
+    int thread_ids [NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_ids[i] = i;
+        pthread_create(&threads[i], NULL, teller_thread, &thread_ids[i]);
+    }
+
+    // Wait for all threads to complete
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Final Account Balances: %f\n", accounts[0].balance);
+
+    double sum = INITIAL_BALANCE; // starting account balance
+    for (int i = 0; i < NUM_THREADS; i++) {
+        for (int j = 0; j < TRANSACTIONS_PER_TELLER; j++) {
+            sum += logs[i][j];
+        }
+    }
+
+    printf("Correct Account Balance: %f\n\n", sum);
+
+    return 0;
+}
